d8bb077f89b7f6e505f0e09f1df45a02dbce3828
marcdahan <marcdahan@gmail.com>
2023-07-18 21:14:39 +0200

    in between

---
+++ src/components/Counter2.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h2>Hello this is for refs man!</h2>
+    <p>You have counted {{this.counter}} times</p>
+    <input type="text" ref="input">
+    <button @click="submit">Add 1 to counter</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Test',
+  data(){
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    submit(){
+      this.counter++;
+      console.log(this.$refs)
+    }
+  }
+}
+</script>
+<style scoped>
+p , input, button{
+  font-size: 30px;
+}
+input, button{
+  font-size: 20px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,4 +1,6 @@
 <script setup>
+import {ref} from 'vue'
+
 // defineProps({
 //   msg: {
 //     type: String,
@@ -8,7 +10,21 @@
 </script>
 
 <template>
-  <div class="greetings"></div>
+  <div class="event-datax">
+    <!-- display event data  -->
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
