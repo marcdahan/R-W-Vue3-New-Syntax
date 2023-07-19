5fe299edbe0153ade9b6f106a93847e0aba47d0e
marcdahan <marcdahan@gmail.com>
2023-07-19 17:43:00 +0200

    css centered fixed

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,24 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style>
+#layout {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+nav {
+  padding: 30px;
+}
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+nav a.router-link-exact-active {
+  color: #42b983;
+}
 h2 {
-  height: 20rem;
+  font-size: 20rem;
 }
 </style>
