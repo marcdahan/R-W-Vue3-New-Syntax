a483a8cdc1a7ba3b68f8ea622a299461564bfc46
marcdahan <marcdahan@gmail.com>
2023-07-29 06:25:48 +0200

    vue devtools rectified + export

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
 import {createApp} from 'vue'
 import {createPinia} from 'pinia'
 
+//devtools connection
+import devtools from '@vue/devtools'
+if ( process.env.NODE_ENV !== 'production' ) {
+  devtools.connect( 'localhost', '5173' )
+}
+
+//Vue app creation
 import App from './App.vue'
 import router from './router'
-
-
 const app = createApp( App )
-
 app.use( createPinia() )
 app.use( router )
-
 const mountedApp = app.mount( '#app' )
+
+//litte export into index.html
 const myVar = `John DOE`
-export default {myVar, mountedApp}
+export {myVar, mountedApp}
