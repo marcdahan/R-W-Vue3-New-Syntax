63c6045f6653150083ae7c4666f326ce47911edf
marcdahan <marcdahan@gmail.com>
2023-11-09 21:34:20 +0100

    new component file added

---
+++ src/components/MyHelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+    msgtransmittedtothecomponent: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+    <div>
+        <h1>{{ msgtransmittedtothecomponent }}</h1>
+        <h3></h3>
+    <p></p>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+</style>
